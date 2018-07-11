e9d56a289b0cef02802ad9969d8abcbf0c643ce1
ania <adam.skudlarski@mail.utoronto.a>
2018-07-11 18:07:48 -0400

    uploading to github

---
+++ subsets.py
@@ -0,0 +1,10 @@
+from itertools import *
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+x = powerset([1,2,3])
+for item in x:
+    print(item)
